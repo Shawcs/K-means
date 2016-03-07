10a38c7c7ab6ce52b686cc481245e71dd445dc13
TakiuGit <paulnoferi@gmail.com>
2016-03-07 13:36:37 +0100

    finish item

--- k-means/k-means/Item.cs
+++ k-means/k-means/Item.cs
@@ -27,5 +27,32 @@ namespace k_means
         {
             this.variables = variables;
         }
+
+        /// <summary>
+        /// Compute the distance between two items based on their variables.
+        /// </summary>
+        /// <param name="b"></param>
+        /// <returns></returns>
+        public double Distance(Item b)
+        {
+            double total = 0;
+            for(int i=0; i < variables.Count; ++i)
+            {
+                total += Math.Pow(variables.ElementAt(i) - b.Variables.ElementAt(0),2.0);
+            }
+            return Math.Sqrt(total);
+        }
+
+        public override string ToString()
+        {
+            StringBuilder strB = new StringBuilder();
+            strB.Append("(");
+
+            foreach(double b in variables)
+            {
+                strB.Append(b).Append(" ; ");
+            }
+            return strB.ToString();
+        }
     }
 }
