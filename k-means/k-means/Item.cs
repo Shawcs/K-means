ce51f73f55b1c3df07f44b876aacab12b07b6958
TakiuGit <paulnoferi@gmail.com>
2016-03-07 13:30:45 +0100

    add iris

---
+++ k-means/k-means/Item.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace k_means
+{
+    class Item
+    {
+        private List<double> variables;
+
+        public List<double> Variables
+        {
+            get
+            {
+                return variables;
+            }
+
+            set
+            {
+                variables = value;
+            }
+        }
+
+        public Item(List<double> variables)
+        {
+            this.variables = variables;
+        }
+    }
+}
