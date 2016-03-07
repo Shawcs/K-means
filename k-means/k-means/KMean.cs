165f6c2d3f246290a047038d393f1eb479acff6b
TakiuGit <paulnoferi@gmail.com>
2016-03-07 14:08:07 +0100

    add gravity center compture(trash)

--- k-means/k-means/KMean.cs
+++ k-means/k-means/KMean.cs
@@ -26,8 +26,27 @@ namespace k_means
         private void compute(List<Item> items)
         {
             this.items = items;
+            initData();
+
+            List<int> newCluster;
+            int y = 0;
+            do
+            {
+                newCluster = new List<int>();
+                seeds = CalculCentroide(cluster, listIris);
+                computeCluster(items, newCluster, seeds);
+                if (IsEquals(newCluster, cluster))
+                    break;
+                cluster = newCluster;
+                Console.WriteLine("Calcul cluster " + y);
+                ++y;
+            } while (true);
+            cluster = newCluster;
         }
 
+        /// <summary>
+        /// Define de k seed
+        /// </summary>
         private void initData()
         {
             Random rand = new Random(); // A tirer aléatoirement
@@ -37,10 +56,71 @@ namespace k_means
                 do
                 {
                     item = items.ElementAt(rand.Next(0, items.Count - 1));
-
                 } while (seeds.Contains(item));
                 seeds.Add(item);
             }
+        }
+
+        private static void computeCluster(List<Item> listIris, List<int> cluster, List<Item> seeds)
+        {
+
+            for (int i = 0; i < listIris.Count; i++)
+            {
+                cluster.Insert(i, LePlusProche(seeds, listIris.ElementAt(i)));
+            }
+        }
+
+        private static int LePlusProche(List<Item> seeds, Item iris)
+        {
+            List<double> distances = new List<double>();
+            for(int i=0; i < seeds.Count;++i)
+            {
+                distances.Add(iris.Distance(seeds.ElementAt(i)));
+            }
+            distances.Min(x);
+        }
+
+        public static List<Item> CalculCentroide(List<int> cluster, List<Item> listIris)
+        {
+            List<Item> result = new List<Item>();
+            for (int i = 0; i < 3; ++i)
+            {
+                int nbInCluser = 0;
+
+                double sumV1 = 0;
+                double sumV2 = 0;
+                double sumV3 = 0;
+                double sumV4 = 0;
+
+                for (int x = 0; x < listIris.Count; ++x)
+                {
+                    if (cluster.ElementAt(x) == i)
+                    {
+                        Item curentIris = listIris.ElementAt(x);
+                        sumV1 += curentIris.V1;
+                        sumV2 += curentIris.V2;
+                        sumV3 += curentIris.V3;
+                        sumV4 += curentIris.V4;
+                        nbInCluser++;
+                    }
+                }
+                result.Add(new Item(sumV1 / nbInCluser, sumV2 / nbInCluser, sumV3 / nbInCluser, sumV4 / nbInCluser, "centroide"));
+            }
+            return result;
+        }
+
+        private static Boolean IsEquals(List<int> list1, List<int> list2)
+        {
+            if (list1.Count == list2.Count)
+            {
+                for (int i = 0; i < list1.Count; ++i)
+                {
+                    if (list1.ElementAt(i) != list2.ElementAt(i))
+                        return false;
+                }
+                return true;
+            }
+            return false;
         }
     }
 }
