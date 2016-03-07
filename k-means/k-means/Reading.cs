bcfc1c616286514f6010744c21e0a1cc21c25848
shawcs <shawcs.t@gmail.com>
2016-03-07 13:52:08 +0100

    reading fix

--- k-means/k-means/Reading.cs
+++ k-means/k-means/Reading.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,16 +11,21 @@ namespace k_means
     {
         public static List<Item> ReadFile()
         {
-
-            const string f = @".\Qualitative_Bankruptcy.data.txt";
-            List<Row> lines = new List<Row>();
+            const string f = @".\wine.data.txt";
+            List<Item> lines = new List<Item>();
             using (StreamReader r = new StreamReader(f))
             {
                 string line;
                 while ((line = r.ReadLine()) != null)
                 {
                     string[] temp = line.Split(',');
-                    lines.Add(new Row(temp));
+                    double[] temps = new double[temp.Length];
+
+                    for (int i = 0; i < temp.Length; i++)
+                    {
+                         temps[i] = double.Parse(temp[i]);
+                    }
+                    lines.Add(new Item(new List<Double>(temps)));
                 }
             }
             Console.WriteLine("file Loaded ! ");
