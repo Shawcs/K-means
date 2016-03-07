20627ab8a2a931bc2aac8bb41f210f409c562462
TakiuGit <paulnoferi@gmail.com>
2016-03-07 13:41:39 +0100

    add wine file + readFile(trash)

---
+++ k-means/k-means/Reading.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace k_means
+{
+    class Reading
+    {
+        public static List<Item> ReadFile()
+        {
+
+            const string f = @".\Qualitative_Bankruptcy.data.txt";
+            List<Row> lines = new List<Row>();
+            using (StreamReader r = new StreamReader(f))
+            {
+                string line;
+                while ((line = r.ReadLine()) != null)
+                {
+                    string[] temp = line.Split(',');
+                    lines.Add(new Row(temp));
+                }
+            }
+            Console.WriteLine("file Loaded ! ");
+            return lines;
+        }
+    }
+}
